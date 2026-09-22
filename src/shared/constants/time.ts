00116b9eeb019f9d82a78ec25e0be67e3b63e480
Abel Mekonen <abelmekonen247@gmail.com>
2026-09-22 03:03:20 +0300

    refactor: update TEN_SECONDS_MS to use multiplication

--- src/shared/constants/time.ts
+++ src/shared/constants/time.ts
@@ -3,4 +3,4 @@ export const ONE_HOUR_MS = 60 * 60 * 1000;
 export const FIFTEEN_MINUTES_MS = 15 * 60 * 1000;
 export const TEN_MINUTES_SECONDS = 10 * 60;
 export const ONE_MINUTE_MS = 60 * 1000;
-export const TEN_SECONDS_MS = 10_000;
+export const TEN_SECONDS_MS = 10 * 1000;
